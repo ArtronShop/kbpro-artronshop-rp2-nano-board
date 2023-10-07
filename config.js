02ee0023147ccf0fd133a51b69d2119b2e250ab7
Max <7087907+maxpromer@users.noreply.github.com>
2023-10-07 22:06:41 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: "artronshop-rp2-nano",
+  platform: "arduino-pico",
+  title: "ArtronShop RP2 Nano",
+  description: "ArtronShop RP2 Nano - Arduino Nano compatible, Powered by RP2040",
+  author: "ArtronShop",
+  website: "http://www.artronshop.co.th",
+  email: "contact@artronshop.co.th",
+  git: "https://github.com/ArtronShop/kbide-artronshop-rp2-nano/",
+  image: "/static/display.jpg",
+  version: "1.1.0"
+};
